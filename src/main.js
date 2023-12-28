5d4a86f7795647cb86a1038ec5178e7c6a779539
srijitcoder <srijitsm123@gmail.com>
2023-12-28 15:14:32 +0530

    added renderer for eox-elements

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import DOMPurify from "isomorphic-dompurify";
 import { html, LitElement } from "lit";
 import { marked } from "marked";
 import { EOxMap } from "@eox/map/dist/eox-map.umd.cjs";
-import { EOxLayerControl } from "@eox/layercontrol/dist/eox-layercontrol.umd.cjs";
+import { EOxLayerControl } from "../../EOxElements/elements/layercontrol/src/main";
+import "@eox/map/dist/eox-map-advanced-layers-and-sources.js";
 
 let ELEMENTS = {
   "eox-map": {
@@ -15,12 +16,13 @@ let ELEMENTS = {
   },
 };
 
+let propertiesKeys = [];
+
 Object.keys(ELEMENTS).forEach((key) => {
   const ele = ELEMENTS[key];
-  console.log(ele.class.elementProperties);
   ele.class.elementProperties.forEach((i, prop) => {
-    if (i.attribute === false && !i.state) {
-      console.log(i.type, i, prop);
+    propertiesKeys = [...propertiesKeys, ...prop];
+    if (!i.attribute && !i.state) {
       ELEMENTS[key].properties = {
         ...ELEMENTS[key].properties,
         [prop]: i.type?.name || "Array",
@@ -58,11 +60,15 @@ function renderHtmlString(htmlString) {
                   case "Number":
                     element[propName] = Number(propValue);
                     break;
+                  case "Boolean":
+                    element[propName] =
+                      propValue.toLowerCase() === "false" ? false : true;
                   case "Array":
                   case "Object":
                     element[propName] = JSON.parse(
                       propValue.replaceAll("&quot;", '"')
                     );
+                    break;
                   default:
                     break;
                 }
@@ -89,14 +95,55 @@ export class Storytelling extends LitElement {
     /**
      * @type {String}
      */
-    this.markdown = `## Map is here
+    this.markdown = `# EOx Storytelling
 ---
-<eox-layercontrol style="width: 100%; height: 300px;" idProperty="id" titleProperty="title" for="eox-map"></eox-layercontrol>
-
-<eox-map style="width: 100%; height: 300px;" zoom="10" center="[15,48]" layers='[{ "type": "Tile", "source": { "type": "OSM" } }]' zoom="7"></eox-map>
+<div style="display: flex">
+<eox-map style="width: 100%; height: 300px;" zoom="3" center="[15,48]" layers='[ { "type": "Group", "properties": { "id": "group2", "title": "Data Layers", "layerControlExpand": true, "description": "# Hello world" }, "layers": [ { "type": "Tile", "properties": { "id": "WIND", "title": "WIND" }, "source": { "type": "TileWMS", "url": "https://services.sentinel-hub.com/ogc/wms/0635c213-17a1-48ee-aef7-9d1731695a54", "params": { "LAYERS": "AWS_VIS_WIND_V_10M" } } }, { "type": "Tile", "properties": { "id": "NO2", "title": "NO2" }, "source": { "type": "TileWMS", "url": "https://services.sentinel-hub.com/ogc/wms/0635c213-17a1-48ee-aef7-9d1731695a54", "params": { "LAYERS": "AWS_NO2-VISUALISATION" } } }, { "type": "Vector", "properties": { "title": "Regions", "id": "regions", "description": "Ecological regions of the earth." }, "source": { "type": "Vector", "url": "https://openlayers.org/data/vector/ecoregions.json", "format": "GeoJSON", "attributions": "Regions: @ openlayers.org" } } ] }, { "type": "Group", "properties": { "id": "group1", "title": "Background Layers" }, "layers": [ { "type": "WebGLTile", "properties": { "id": "s2", "layerControlExclusive": true, "title": "s2" }, "style": { "variables": { "red": 1, "green": 2, "blue": 3, "redMax": 3000, "greenMax": 3000, "blueMax": 3000 }, "color": [ "array", [ "/", [ "band", [ "var", "red" ] ], [ "var", "redMax" ] ], [ "/", [ "band", [ "var", "green" ] ], [ "var", "greenMax" ] ], [ "/", [ "band", [ "var", "blue" ] ], [ "var", "blueMax" ] ], 1 ], "gamma": 1.1 }, "source": { "type": "GeoTIFF", "normalize": false, "sources": [ { "url": "https://s2downloads.eox.at/demo/EOxCloudless/2020/rgbnir/s2cloudless2020-16bits_sinlge-file_z0-4.tif" } ] } }, { "type": "Tile", "properties": { "id": "osm", "title": "Open Street Map", "layerControlExclusive": true }, "visible": false, "opacity": 0.5, "source": { "type": "OSM" } } ] } ]' zoom="7"></eox-map>
+<eox-layercontrol style="width: 100%; height: 300px;" idProperty="id" titleProperty="title" unstyled="false" for="eox-map"></eox-layercontrol>
+</div>
 
 ---
-**caption here**    
+**caption here**   
+
+# EOxElements
+
+A Web Component collection of geospatial UI elements, crafted by EOX.
+
+## Documentation, Examples
+
+Please find [descriptions, API docs and interactive examples here](https://eox-a.github.io/EOxElements).
+
+## Elements
+
+- ⭕️ **Alpha** elements are in-development and may have many frequent breaking
+  changes.
+- 🟡 **Beta** elements are mostly polished and ready for use, but may still have
+  breaking changes.
+- ✅ **Stable** elements are reviewed, documented, and API complete.
+
+<table>
+  <tr>
+    <th>Element</th>
+    <th>Description</th>
+    <th>Docs & Examples</th>
+    <th>Version</th>
+    <th>State</th>
+  </tr>
+  <tr>
+    <td><a href="./elements/chart/">eox-chart</a></td>
+    <td>Dynamic chart with built-in data fetching</td>
+    <td><a href="https://eox-a.github.io/EOxElements/index.html?path=/docs/elements-eox-chart--docs">Docs & Examples</a></td>
+    <td><a href="elements/chart/CHANGELOG.md"><img src="https://img.shields.io/npm/v/@eox/chart.svg?label=%20" /></a></td>
+    <td>⭕️</td>
+  </tr>
+  <tr>
+    <td><a href="./elements/drawtools/">eox-drawtools</a></td>
+    <td>Draw and manage features on a map</td>
+    <td><a href="https://eox-a.github.io/EOxElements/index.html?path=/docs/elements-eox-drawtools--docs">Docs & Examples</a></td>
+    <td><a href="elements/drawtools/CHANGELOG.md"><img src="https://img.shields.io/npm/v/@eox/drawtools.svg?label=%20" /></a></td>
+    <td>🟡</td>
+  </tr>
+</table>
 `;
   }
 
@@ -105,7 +152,7 @@ export class Storytelling extends LitElement {
     this.#html = DOMPurify.sanitize(parsedHtml, {
       CUSTOM_ELEMENT_HANDLING: {
         tagNameCheck: /^eox-/,
-        attributeNameCheck: /style|zoom|center|layers/,
+        attributeNameCheck: new RegExp(propertiesKeys.join("|")),
         allowCustomizedBuiltInElements: true,
       },
     });
@@ -123,6 +170,10 @@ export class Storytelling extends LitElement {
   firstUpdated() {
     this.parseHTML();
     this.requestUpdate();
+  }
+
+  createRenderRoot() {
+    return this;
   }
 
   render() {
@@ -167,6 +218,10 @@ export class Storytelling extends LitElement {
         height: 100%;
         border: 0;
         outline: 0;
+        font-family: 'Courier New', monospace;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 140%;
       }
       .empty-preview {
         display: flex;
