f29c4d6dc09829782394e65f790550b918223ef3
srijitcoder <srijitsm123@gmail.com>
2024-01-24 23:11:54 +0530

    fix class conflict of editor and switch by changing class name

--- src/components/editor.js
+++ src/components/editor.js
@@ -120,7 +120,7 @@ class StoryTellingEditor extends LitElement {
             @click=${this.#switchEditorView}
             .checked=${this.#temporaryEnableEditor ? true : false}
           />
-          <span class="slider round"></span>
+          <span class="switch-slider round"></span>
           <i class="icon editor-icon"></i>
         </label>
       </div>
@@ -220,7 +220,7 @@ class StoryTellingEditor extends LitElement {
     .switch .icon.editor-icon:before {
       content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ctitle%3Epencil%3C/title%3E%3Cpath fill='white' d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /%3E%3C/svg%3E");
     }
-    .slider {
+    .switch-slider {
       position: absolute;
       cursor: pointer;
       top: 0;
@@ -235,7 +235,7 @@ class StoryTellingEditor extends LitElement {
       right: 0px;
       width: 65px;
     }
-    .slider:before {
+    .switch-slider:before {
       cursor: pointer;
       position: absolute;
       content: "";
@@ -247,10 +247,10 @@ class StoryTellingEditor extends LitElement {
       transition: .4s;
       border-radius: 50%;
     }
-    .switch-input:checked + .slider {
+    .switch-input:checked + .switch-slider {
       background-color: #2196F3;
     }
-    .switch-input:checked + .slider:before {
+    .switch-input:checked + .switch-slider:before {
       transform: translateX(30px);
     }
     .switch .icon.editor-view {
